e3ddedf7aabf392dc6dd64f587895311f662269f
gcerv93 <gcerv93@protonmail.com>
2022-02-12 14:18:43 -0600

    update display_board method to handle available moves

--- lib/display.rb
+++ lib/display.rb
@@ -13,12 +13,35 @@ module Display
     print string.colorize(background: :white) if index.odd?
   end
 
-  def display_board
+  # rubocop: disable Metrics
+  # This works for now. Is it too complex???
+  def display_board(moves = [])
     n = 8
     board.each.with_index do |row, idx|
       print n
       row.each.with_index do |_cell, i|
-        idx.even? ? print_even_rows(row[i].to_s, i) : print_odd_rows(row[i].to_s, i)
+        # convert the object to a string. Assign it to a variable to keep track of
+        # it during the current iteration
+        spot = row[i].to_s
+
+        # There's 3 include? checks here
+        # The first one checks if the spot is included in the array of moves, and if its
+        # a 3-whitespace-string. Only these spots get filled with pink circles
+        # Remove from moves array for future checks
+        if moves.include?([idx, i]) && spot == '   '
+          spot = " \u25cf ".colorize(:light_red)
+          moves.delete([idx, i])
+        end
+
+        # send the rows to the print methods. the unless check is for the spots that survived the
+        # above include? check.
+        idx.even? ? print_even_rows(spot, i) : print_odd_rows(spot, i) unless moves.include?([idx, i])
+
+        # Those spots get handled here, where we print the spots skipped in the above unless check
+        if moves.include?([idx, i])
+          print spot.colorize(background: :light_red)
+          moves.delete([idx, i])
+        end
       end
       print "\n"
       n -= 1
@@ -26,3 +49,4 @@ module Display
     puts '  A  B  C  D  E  F  G  H'
   end
 end
+# rubocop: enable Metrics
